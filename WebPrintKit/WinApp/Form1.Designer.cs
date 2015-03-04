6a009f80d098b076d98e1d2f240bdb1f8e6bd43f
徐松涛 <kally32@163.com>
2015-03-04 16:58:22 +0800

    1

--- WebPrintKit/WinApp/Form1.Designer.cs
+++ WebPrintKit/WinApp/Form1.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ocx = new Lib.OCX();
+            this.ocx1 = new Lib.OCX();
+            this.SuspendLayout();
+            // 
+            // ocx
+            // 
+            this.ocx.CopiesValue = "1";
+            this.ocx.DuplexValue = "单面";
+            this.ocx.InstalledPrintersValue = "HP LaserJet P2050 Series PCL6";
+            this.ocx.LandscapeValue = "纵向";
+            this.ocx.Location = new System.Drawing.Point(-1, 0);
+            this.ocx.Name = "ocx";
+            this.ocx.NoImageUrls = "";
+            this.ocx.PaperValue = "A4";
+            this.ocx.Urls = "http://172.31.132.44:8001/FarmPlat/CMDSManage/FMDS_FarmFarmerCollection/FMDS_FarmFarmerCollectionDetailB.aspx?IsHouseholder=0&ID=9b15683b-d99a-4a7b-b2f4-7cd6bf6654db&CollectionYear=2014&WorkID=533|http://172.31.132.44:8001/FarmPlat/CMDSManage/FMDS_FarmFarmerCollection/FMDS_FarmFarmerCollectionDetailB.aspx?IsHouseholder=0&ID=9b15683b-d99a-4a7b-b2f4-7cd6bf6654db&CollectionYear=2014&WorkID=533";
+            this.ocx.IsHeightFixed = "0";
+            this.ocx.Size = new System.Drawing.Size(1300, 670);
+            this.ocx.TabIndex = 0;
+            // 
+            // ocx1
+            // 
+            this.ocx1.CopiesValue = null;
+            this.ocx1.DuplexValue = null;
+            this.ocx1.InstalledPrintersValue = null;
+            this.ocx1.LandscapeValue = null;
+            this.ocx1.Location = new System.Drawing.Point(-1, 0);
+            this.ocx1.Name = "ocx1";
+            this.ocx1.NoImageUrls = "";
+            this.ocx1.PaperValue = null;
+            this.ocx1.Size = new System.Drawing.Size(1300, 670);
+            this.ocx1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 510);
+            this.Controls.Add(this.ocx);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Lib.OCX ocx;
+        private Lib.OCX ocx1;
     }
 }
 
